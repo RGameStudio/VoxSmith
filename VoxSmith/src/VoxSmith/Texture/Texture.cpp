1eb2f17af3ae0c9bfb5509504ca49da562d2d959
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2025-01-07 19:16:30 +0300

    Texture mipmapping was added

--- VoxSmith/src/VoxSmith/Texture/Texture.cpp
+++ VoxSmith/src/VoxSmith/Texture/Texture.cpp
@@ -24,7 +24,7 @@ Texture::Texture(std::vector<std::string> paths)
 	glGenTextures(1, &ID);
 	glBindTexture(GL_TEXTURE_2D_ARRAY, ID);
 
-	glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, GL_RGBA8, g_width, g_height, paths.size());
+	glTexStorage3D(GL_TEXTURE_2D_ARRAY, 7, GL_RGBA8, g_width, g_height, paths.size());
 	for (uint8_t iPath = 0; iPath < paths.size(); iPath++)
 	{
 		int32_t width, height, nrChannels;
@@ -58,13 +58,17 @@ Texture::Texture(std::vector<std::string> paths)
 			transparency,
 			GL_UNSIGNED_BYTE,
 			data);
+
+
 		stbi_image_free(data);
 	}
+	
+	glGenerateMipmap(GL_TEXTURE_2D_ARRAY);
 
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
 }
 
 Texture::Texture(const uint32_t width, const uint32_t height)
